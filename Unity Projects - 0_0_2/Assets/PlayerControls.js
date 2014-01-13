c9b5e389c9564051ee0bdc97ac17c6b30b2b5631
Adam Edelstein <adam.ce@gmail.com>
2014-01-13 12:27:09 -0500

    update for 0.0.2

---
+++ Unity Projects - 0_0_2/Assets/PlayerControls.js
@@ -0,0 +1,42 @@
+﻿#pragma strict
+
+var moveRight : KeyCode;
+var moveLeft : KeyCode;
+var moveUp : KeyCode;
+var moveDown : KeyCode;
+
+var speed : float = 3;
+
+function Update () {
+
+	if (Input.GetKey(moveRight))
+	{
+		rigidbody2D.velocity.x = speed;
+	}
+	else if (Input.GetKey(moveLeft))
+	{
+		rigidbody2D.velocity.x = speed *-1;
+	}
+	else
+	{
+	rigidbody2D.velocity.x = 0;
+	}
+	
+	if (Input.GetKey(moveUp))
+	{
+		rigidbody2D.velocity.y = speed;
+	}
+	else if (Input.GetKey(moveDown))
+	{
+		rigidbody2D.velocity.y = speed *-1;
+	}
+	else
+	{
+	
+		rigidbody2D.velocity.y = 0;
+	}
+
+}
+
+
+
---
+++ Unity Projects - 0_0_2/Assets/enemy.js
@@ -0,0 +1,224 @@
+﻿#pragma strict
+
+
+// Enemy Controller
+// Description: Control component enemy logic, options and properties
+	
+var moveSpeed 		: float 	= 20.0;	 // set the speed of the enemy
+var attackMoveSpeed : float 	= 35.0;	 // set the speed for attack range
+var jumpSpeed 		: float 	= 3.0; 	 // set the jump height
+	
+enum EnemyState 	{ moveLeft = 0, moveRight = 1, moveUp = 2, moveDown = 3, idleLeft = 5, idleRight = 4, jumpAir = 6, enemyDie = 7, goHome = 8 }
+var enemyState = EnemyState.moveLeft; 	 // set the starting state
+
+var attackRange		: float 	= 1.0; 	 // set the range for the attackMoveSpeed attack/movement speed
+var searchRange 	: float 	= 3.0; 	 // set the range for finding the player
+var returnHome   	: float 	= 4.0; 	 // set leashing distance
+	
+var chaseTarget 	: Transform; 		 // load up the player target
+var homePosition 	: Transform; 		 // load up the home position
+var deathForce 		: float 	= 3.0; 	 // when the player jumps on me force him off 'x' amount
+	
+var gizmoToggle 	: boolean = true; 	 // toggle the display of the debug radius
+	
+private var velocity 		 : Vector3 = Vector3.zero; 	// store the player movement in velocity (x, y)
+//private var gravity 		 : float = 20.0; 			// weight of the world pushing the enemy down
+private var currentState;								// hold the current state for setting later
+private var aniPlay;									// get the component for animations
+private var isRight 		 : boolean = false;			// setting the direction
+private var myTransform 	 : Vector3;					// store initial position
+private var resetMoveSpeed   : float = 0.0;				// store the initial move speed
+private var distanceToHome   : float = 0.0;				// get the distance to the inital move position
+private var distanceToTarget : float = 0.0;				// get distance to the target position
+//private var controller 		 : CharacterController; 	// get controller 
+private var controller 		 : Rigidbody2D; 	// get controller 
+
+
+
+var animator : Animator;
+
+
+function Start () 
+{	
+	myTransform = transform.position;
+	resetMoveSpeed = moveSpeed;
+	controller = GetComponent (Rigidbody2D);
+
+//	linkToPlayerProperties = GetComponent(playerProperties);  // This links to player properties which includes (lives, states, etc)
+//	aniPlay = GetComponent (aniSprite); // Some sort of animation sprite thing, possibly linked to the player
+
+	animator = GetComponent("Animator");	
+}	
+	
+function Update () 
+{
+	
+		switch (enemyState)
+		{
+			case EnemyState.moveLeft :
+			PatrolLeft ();
+			animator.SetInteger("Direction", 0);
+			break; 	
+			
+			case EnemyState.moveRight :
+			PatrolRight ();
+			animator.SetInteger("Direction", 1);
+			break; 	
+			
+			case EnemyState.moveUp :
+			PatrolUp ();
+			animator.SetInteger("Direction", 2);
+			break; 	
+			
+			case EnemyState.moveDown :
+			PatrolDown ();
+			animator.SetInteger("Direction", 3);
+			break; 
+			
+			case EnemyState.idleRight :
+			IdleRight (); 
+			animator.SetInteger("Direction", 4);
+			
+			case EnemyState.idleLeft :
+			IdleLeft ();
+			animator.SetInteger("Direction", 5);
+				
+			case EnemyState.jumpAir :
+ 			JumpRight ();
+			break; 	
+			
+			case EnemyState.enemyDie :
+			DieRight ();
+			break; 		
+			
+			case EnemyState.goHome :
+			GoHome ();
+			break;										
+		}
+	
+	//Apply gravity
+	//	velocity.y -= gravity * Time.deltaTime;			// apply the gravity
+	// move the controller
+	//rigidbody2D.Move (velocity * Time.deltaTime); 	// move the controller
+}
+
+function OnTriggerEnter (other : Collider)
+{
+
+}
+// move the enemy right
+function PatrolRight ()
+{
+	rigidbody2D.velocity.x = moveSpeed * Time.deltaTime;
+	animator.SetInteger("Direction", 0);
+	isRight = true;
+}
+
+// move the enemy left
+function PatrolLeft ()
+{
+	rigidbody2D.velocity.x = -moveSpeed * Time.deltaTime;		// move the controller to the left	
+	animator.SetInteger("Direction", 1);
+	isRight = false;
+}
+
+// move the enemy up
+function PatrolUp ()
+{
+	rigidbody2D.velocity.y = moveSpeed * Time.deltaTime;		// move the controller to the left	
+	animator.SetInteger("Direction", 2);
+}
+
+// move the enemy down
+function PatrolDown ()
+{
+	rigidbody2D.velocity.y = -moveSpeed * Time.deltaTime;		// move the controller to the left	
+	animator.SetInteger("Direction", 3);
+}
+
+// set movement to 0 and face right
+function IdleRight ()
+{
+	rigidbody2D.velocity.x = 0;
+	animator.SetInteger("Direction", 4);
+	isRight = true;
+	
+}
+
+// set movement to 0 and face left
+function IdleLeft ()
+{
+	rigidbody2D.velocity.x = 0;
+	animator.SetInteger("Direction", 5);
+	isRight = false;
+}
+
+// jumps in the air to the right
+function JumpRight ()
+{
+
+}
+
+// jumps in the air to the left
+function JumpLeft ()
+{
+
+}
+
+// kill the enemy right
+function DieRight ()
+{ 
+
+}
+
+// kill enemy left
+function DieLeft ()
+{
+
+}
+
+// check for where the player is in relation to our position
+function ChasePlayer ()
+{
+
+}
+
+// finds the home node and returns to it
+function GoHome ()
+{
+
+}
+
+// toggle the gizmos for the designer to see ranges
+function OnDrawGizmos ()
+{
+
+}
+
+private class ShushES {var q:Queue; var h:Help; var g:GUI;}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
